e79cc22d8f3eac4239cc4d866dc9f851364b8bc9
Vitor Wagner <vitormwagner@gmail.com>
2023-05-10 22:34:48 -0300

    Fix: :bug: Fixed matches router issue with token middleware

--- app/backend/src/routes/matches.router.ts
+++ app/backend/src/routes/matches.router.ts
@@ -1,16 +1,18 @@
 import { Router } from 'express';
+import tokenValidation from '../middlewares/tokenValidation';
 import verifyNewMatch from '../middlewares/verifyNewMatch';
 import MatchesController from '../controllers/matches.controller';
-import tokenValidation from '../middlewares/tokenValidation';
 
 const router = Router();
 
 const matchesController = new MatchesController();
 
 router.get('/', (req, res) => matchesController.getAll(req, res));
-router.post('/', verifyNewMatch, (req, res) => matchesController.create(req, res));
-router.use(tokenValidation);
-router.patch('/:id', (req, res) => matchesController.update(req, res));
-router.patch('/:id/finish', (req, res) => matchesController.finish(req, res));
+
+// router.use(tokenValidation);
+
+router.post('/', tokenValidation, verifyNewMatch, (req, res) => matchesController.create(req, res));
+router.patch('/:id', tokenValidation, (req, res) => matchesController.update(req, res));
+router.patch('/:id/finish', tokenValidation, (req, res) => matchesController.finish(req, res));
 
 export default router;
